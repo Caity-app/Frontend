7434fb03e55b0c17909fb713ddab9ce3abe86b67
gllms <willemvanderdoe@hotmail.com>
2022-10-20 04:28:53 +0200

    Improve touch support

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,13 +1,12 @@
-import { SetStateAction, Dispatch, MutableRefObject, useRef, forwardRef, useImperativeHandle } from 'react';
+import { useRef, forwardRef, useImperativeHandle } from 'react';
 import SideBarItem from './SideBarItem';
-import { CalendarDaysIcon, Cog6ToothIcon, ShoppingBagIcon, Squares2X2Icon, UserCircleIcon } from '@heroicons/react/24/outline';
+import { CalendarDaysIcon, Cog6ToothIcon, ShoppingBagIcon, Squares2X2Icon } from '@heroicons/react/24/outline';
 
 interface SideBarProps {
-    setSideBarIsOpen: Dispatch<SetStateAction<boolean>>;
     sideBarIsOpen: boolean;
 }
 
-const SideBar = forwardRef(({ setSideBarIsOpen, sideBarIsOpen } : SideBarProps, ref ) => {
+const SideBar = forwardRef(({ sideBarIsOpen } : SideBarProps, ref ) => {
 
     const backdrop = useRef(null);
     const menu = useRef(null);
@@ -22,7 +21,7 @@ const SideBar = forwardRef(({ setSideBarIsOpen, sideBarIsOpen } : SideBarProps, 
     }));
 
     return (
-        <div className='h-screen fixed transition-transform pointer-events-none'>
+        <div className='h-screen fixed transition-transform pointer-events-none' onDragStart={e => e.preventDefault()}>
             <div ref={backdrop} className={'w-screen h-full transition-colors bg-black/20 opacity-0' + (sideBarIsOpen ? ' pointer-events-auto opacity-100': ' pointer-events-none')}></div>
             <div ref={menu} className={'w-56 h-full absolute left-0 top-0 pt-14 bg-zinc-800 shadow-xl transition-transform pointer-events-auto select-none' + (sideBarIsOpen ? '' : ' -translate-x-full')}>
                 <ul className='flex flex-col justify-between h-full pb-4'>
