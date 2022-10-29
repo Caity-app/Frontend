1e2df6e67721a2cbe6819236cf6cb226377ed18f
Ovvugt <51384692+Ovvugt@users.noreply.github.com>
2022-10-29 16:04:42 +0200

    WIP: Add functionality to update quantity

--- src/pages/GroceryList.tsx
+++ src/pages/GroceryList.tsx
@@ -1,34 +1,52 @@
+import { useState } from 'react'
 import GroceryListItem from '../components/GroceryListItem'
+import { GroceryItem } from '../types/GroceryItem'
 
-const mockGroceries = [
+const mockGroceries: GroceryItem[] = [
     {
+        id: 1,
         itemName: 'Apple',
         quantity: 2
     },
     {
+        id: 2,
         itemName: 'Pear',
         quantity: 4
     },
     {
+        id: 3,
         itemName: 'Banana',
         quantity: 5
     },
     {
+        id: 4,
         itemName: 'Orange',
         quantity: 2
     },
     {
+        id: 5,
         itemName: 'Strawberry',
         quantity: 37
     }
 ]
 
 const GroceryList = () => {
+    let [groceries, setGroceries] = useState(mockGroceries);
+
+    const handleQuantityChange = (item: GroceryItem, quantity: number) => {
+        let newGroceries = [...groceries];
+        let index = newGroceries.findIndex(grocery => grocery.id === item.id);
+        newGroceries[index].quantity = quantity;
+        if (quantity === 0) {
+            newGroceries.splice(index, 1);
+        }
+        setGroceries(newGroceries);
+    }
     return (
         <div className='flex flex-col w-full h-full gap-2'>
             <h1 className='text-center'>Grocery List</h1>
             <div className='w-full overflow-y-scroll rounded-md'>
-                {mockGroceries.map((groceryItem) => <GroceryListItem {...groceryItem}/>)}
+                {groceries.map((item) => <GroceryListItem key={`${item.id}:${item.quantity}`} groceryItem={item} handleQuantityChange={handleQuantityChange}/>)}
             </div>
             <div className='flex w-full basis-12 mt-4 rounded-md justify-around'>
                 <button className='bg-sky-500 text-white text-center px-4 rounded-full shadow-md'>
@@ -37,7 +55,6 @@ const GroceryList = () => {
                 <button className='bg-sky-500 text-white text-center px-4 rounded-full shadow-md'>
                     Add manually
                 </button>
-
             </div>
         </div>
     )
