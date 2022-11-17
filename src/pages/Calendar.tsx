5ac8c0eba6f1f9dd70f1db6ebbf3220b85c670d7
gllms <willemvanderdoe@hotmail.com>
2022-11-17 20:56:25 +0100

    WIP: Improve animations and add new event form

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,6 +1,6 @@
 import { Dispatch, useState } from 'react';
-import { ArrowLeftIcon, ArrowRightIcon, PlusIcon } from '@heroicons/react/24/outline'
-import { motion, AnimatePresence } from 'framer-motion';
+import { ArrowLeftIcon, ArrowRightIcon, ClockIcon, DocumentIcon, PencilIcon, PlusIcon } from '@heroicons/react/24/outline'
+import { motion, AnimatePresence, Variants } from 'framer-motion';
 
 interface IEvent {
     date: string,
@@ -10,11 +10,24 @@ interface IEvent {
 
 const events: IEvent[] = [
     {
-        date: '10-06-2022',
+        date: '11-06-2022',
         title: 'Event 1',
         description: 'This is the first event'
     }
 ];
+
+const variants: Variants = {
+    closed: {
+        height: 0,
+        overflow: 'hidden'
+    },
+    open: {
+        height: 'auto',
+        transitionEnd: {
+            overflow: 'visible'
+        }
+    }
+}
 
 const today = new Date();
 
@@ -29,15 +42,16 @@ function range(start: number, end?: number): number[] {
 }
 
 interface DayProps {
-    day: number;
+    number: number;
+    layoutId: string;
     setSelectedDay?: Dispatch<React.SetStateAction<number>>;
     className?: string;
 }
 
-function Day({ day, setSelectedDay, className }: DayProps) {
-    return <div onClick={() => setSelectedDay?.(i => i === day ? -1 : day)} className={'rounded-full aspect-square flex justify-center items-center text-sm text-white select-none' + (className ? ' ' + className : '')}>
-        {day}
-    </div>;
+function Day({ number, layoutId, setSelectedDay, className }: DayProps) {
+    return <motion.div layoutId={layoutId} onClick={() => setSelectedDay?.(i => i === number ? -1 : number)} className={`rounded-full aspect-square flex justify-center items-center m-1 text-sm text-white select-none ${className ?? ''}`}>
+        {number}
+    </motion.div>;
 }
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -45,8 +59,9 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 export default function Calendar() {
     const [currentMonth, setCurrentMonth] = useState(today.getMonth());
     const [currentYear, setCurrentYear] = useState(today.getFullYear());
-
+    
     const [selectedDay, setSelectedDay] = useState(-1);
+    const [isCreatingEvent, setIsCreatingEvent] = useState(false);
 
     const prevMonth = currentMonth === 0 ? 11 : currentMonth - 1;
     const nextMonth = currentMonth === 11 ? 0 : currentMonth + 1;
@@ -54,17 +69,16 @@ export default function Calendar() {
     const prevLastDay = getDaysInMonth(prevMonth, currentYear);
     const lastDay = getDaysInMonth(currentMonth, currentYear);
 
-    // find last monday of the month
-    let lastMonday = prevLastDay;
-    while ((new Date(currentYear, prevMonth, lastMonday).getDay()) !== 1) {
-        lastMonday--;
-    }
+    const lastMonday = prevLastDay - new Date(currentYear, prevMonth, prevLastDay).getDay() + 1;
 
-    let i = 0;
+    const prevMonthYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+    const nextMonthYear = currentMonth === 11 ? currentYear + 1 : currentYear;
 
-    let prevDays = range(lastMonday, prevLastDay).map(day => <Day day={day} className='text-zinc-500' />);
-    const currentDays = range(1, lastDay).map(day => <Day day={day} setSelectedDay={setSelectedDay} className={'bg-zinc-700 text-white cursor-pointer' + ((events.find(e => new Date(e.date).getTime() === new Date(currentYear, currentMonth, day).getTime()) && ' text-sky-500') || '') + (selectedDay === day ? ' !bg-sky-500 !text-white' : '')} />);
-    let nextDays = range(1, 7 - (prevDays.length + lastDay) % 7).map(day => <Day day={day} className='text-zinc-500' />);
+    let gridIndex = 0;
+
+    let prevDays = range(lastMonday, prevLastDay).map(n => <Day number={n} className='text-zinc-500' layoutId={'grid-' + gridIndex++} />);
+    const currentDays = range(1, lastDay).map(n => <Day number={n} setSelectedDay={setSelectedDay} className={`bg-zinc-700 text-white cursor-pointer${events.find(e => new Date(e.date).getTime() === new Date(currentYear, currentMonth, n).getTime()) ? ' text-sky-500' : ''} ${selectedDay === n ? ' !bg-sky-500 !text-white' : ''}`} layoutId={'grid-' + gridIndex++} />);
+    let nextDays = range(1, 7 - (prevDays.length + lastDay) % 7).map(n => <Day number={n} className='text-zinc-500' layoutId={'grid-' + gridIndex++} />);
 
     if (prevDays.length === 7)
         prevDays = [];
@@ -79,41 +93,66 @@ export default function Calendar() {
     for (let i = 0; i < days.length; i += 7) {
         weeks.push(days.slice(i, i + 7));
         weeks.push(
-            <AnimatePresence>
+            <AnimatePresence key={'presence-' + i}>
                 {(selectedDay > -1 && (selectedDay + prevDays.length - 1) >= i && (selectedDay + prevDays.length - 1) < i + 7) && <motion.div
-                    initial={{ height: 0 }}
-                    animate={{ height: "auto" }}
-                    exit={{ height: 0 }}
-                    transition={{
-                        duration: .1
-                    }}
-                    layout
-                    className="col-span-7 overflow-hidden"
+                    variants={variants}
+                    initial='closed'
+                    animate='open'
+                    exit='closed'
+                    transition={{ type: 'tween' }}
+                    className='col-span-7'
                 >
                     <div className='flex flex-col gap-3 my-1'>
-                        {events.filter(e => new Date(e.date).getTime() === new Date(currentYear, currentMonth, selectedDay).getTime()).map(event => <div className='bg-zinc-700 p-4 rounded-3xl'>{event.title}</div>)}
-                        <button className='p-4 bg-zinc-700 text-white rounded-3xl'>
-                            <PlusIcon className='mx-auto' />
-                        </button>
+                        {events.filter(e => new Date(e.date).getTime() === new Date(currentYear, currentMonth, selectedDay).getTime()).map((event, i) => <motion.div layout className='bg-zinc-700 p-4 rounded-3xl' key={'event-' + i} layoutId={'event-' + i}>{event.title}</motion.div>)}
+
+                        <motion.div layout onClick={() => setIsCreatingEvent(true)} className='flex flex-col gap-2 p-4 bg-zinc-700 text-white overflow-hidden' style={{ borderRadius: '24px' }} >
+                            {isCreatingEvent ?
+                                <>
+                                    <motion.label layout className='flex gap-2'>
+                                        <ClockIcon />
+                                        <input type='time' className='w-full p-2 px-4 bg-zinc-600 rounded-full' placeholder='title' />
+                                    </motion.label>
+                                    <motion.label layout className='flex gap-2'>
+                                        <PencilIcon />
+                                        <input className='w-full p-2 px-4 bg-zinc-600 rounded-full' placeholder='title' />
+                                    </motion.label>
+                                    <motion.label layout className='flex gap-2'>
+                                        <DocumentIcon />
+                                        <textarea className='w-full p-2 px-4 bg-zinc-600 rounded-3xl' placeholder='title' />
+                                    </motion.label>
+                                    <motion.button layout="position" layoutId="add-event-button" onClick={e => { setIsCreatingEvent(false); e.stopPropagation() }} className='w-fit mx-auto p-2 px-6 bg-sky-500 rounded-full'>
+                                        Cancel
+                                    </motion.button>
+                                </>
+                                :
+                                <motion.span layout="position" layoutId="add-event-button">
+                                    <PlusIcon className='mx-auto' />
+                                </motion.span>
+                            }
+                        </motion.div>
                     </div>
                 </motion.div>}
-            </AnimatePresence>
+            </AnimatePresence >
         );
     }
 
     return (
         <div className='flex flex-col items-center w-full'>
             <div className='flex justify-center w-full'>
-                <div className="grid grid-cols-7 items-center w-full max-w-sm h-fit gap-2">
-                <button className='flex items-center justify-center navButton aspect-square bg-sky-500 text-white rounded-full' onClick={() => setCurrentMonth(prevMonth)}>
-                    <ArrowLeftIcon className='w-4 h-4' />
-                </button>
-                <p className='col-span-5 text-white text-center text-xl'>{months[currentMonth]}</p>
-                <button className='flex items-center justify-center navButton aspect-square bg-sky-500 text-white rounded-full' onClick={() => setCurrentMonth(nextMonth)}>
-                    <ArrowRightIcon className='w-4 h-4' />
-                </button>
-                    {['M', 'T', 'W', 'T', 'F', 'S', 'S'].map(day => <div className='text-center mt-2'>{day}</div>)}
-                    {weeks}
+                <div className="w-full max-w-sm h-fit">
+                    <div className='grid grid-cols-7 items-center gap-2 mb-2'>
+                        <button className='flex items-center justify-center navButton aspect-square bg-sky-500 text-white rounded-full' onClick={() => { setCurrentMonth(prevMonth); setCurrentYear(prevMonthYear)  }}>
+                            <ArrowLeftIcon className='w-4 h-4' />
+                        </button>
+                        <p className='col-span-5 text-white text-center text-xl'>{months[currentMonth]}</p>
+                        <button className='flex items-center justify-center navButton aspect-square bg-sky-500 text-white rounded-full' onClick={() => { setCurrentMonth(nextMonth); setCurrentYear(nextMonthYear) }}>
+                            <ArrowRightIcon className='w-4 h-4' />
+                        </button>
+                        {['M', 'T', 'W', 'T', 'F', 'S', 'S'].map((day, i) => <div className='text-center' key={'day-name-' + i}>{day}</div>)}
+                    </div>
+                    <motion.div layout className='grid grid-cols-7 items-center gap-0'>
+                        {weeks}
+                    </motion.div>
                 </div>
             </div>
         </div >
